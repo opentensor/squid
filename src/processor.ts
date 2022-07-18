c992f0287a55eed1c65395a632f66239d4fabb06
Carro <robertmyers@hey.com>
2022-07-18 14:06:39 -0500

    forgot to add processor.run()

--- src/processor.ts
+++ src/processor.ts
@@ -1,30 +1,99 @@
 import * as ss58 from "@subsquid/ss58"
 import { lookupArchive } from "@subsquid/archive-registry"
-import {EventHandlerContext, SubstrateProcessor} from "@subsquid/substrate-processor"
+import {BatchContext, BatchProcessorItem, SubstrateBatchProcessor} from "@subsquid/substrate-processor"
 import {Store, TypeormDatabase} from "@subsquid/typeorm-store"
 import { Account } from "./model";
 
-const processor = new SubstrateProcessor(new TypeormDatabase());
+
+const processor = new SubstrateBatchProcessor()
+    .setBatchSize(500)
+    .setTypesBundle('types.json')
+    .setDataSource({
+        // For locally-run archives:
+        archive: 'http://206.81.4.77:8888/graphql'
+        // Lookup archive by the network name in the Subsquid registry
+        // archive: lookupArchive("kusama", { release: "FireSquid" })
+    })
+    .addEvent('Balances.Transfer', {
+        data: {event: {args: true}}
+    } as const)
 
 
-processor.setBatchSize(500);
-processor.setDataSource({
-  archive: 'http://206.81.4.77:4350/graphql',
-  chain: "ws://archivelb.nakamoto.opentensor.ai:9944",
-  
-});
+type Item = BatchProcessorItem<typeof processor>
+type Ctx = BatchContext<Store, Item>
 
-processor.setTypesBundle('types.json');
+const logger = ({data}: any) => {
+    console.log(data)
+}
 
-const logger = (data: any) => {
-  console.log(data);
+interface TransferEvent {
+    from: Uint8Array
+    to: Uint8Array
+    amount: bigint
 }
 
 
-processor.addEventHandler('Balances.Transfer', async ctx => {
-    const event = ctx.event;
-    
-    logger(event);
+
+function getTransferEvent(ctx: Ctx) {
+    console.log(ctx)
+}
+
+
+processor.run(new TypeormDatabase(), async ctx => {
+    ctx.log.info(ctx)
 })
 
-processor.run();
+
+// interface TransferEvent {
+//     id: string
+//     from: string
+//     to: string
+//     amount: bigint
+//     timestamp: bigint
+// }
+
+
+// function getRegistrations(ctx: Ctx): Registration[] {
+//     return ctx.store.find(Registration)
+// } 
+
+// function getTransfers(ctx: Ctx): TransferEvent[] {
+//     let transfers: TransferEvent[] = []
+//     for (let block of ctx.blocks) {
+//         for (let item of block.items) {
+//             if (item.name == "Balances.Transfer") {
+//                 let e = new BalancesTransferEvent(ctx, item.event)
+//                 let rec: {from: Uint8Array, to: Uint8Array, amount: bigint}
+//                 if (e.isV1020) {
+//                     let [from, to, amount, ] = e.asV1020
+//                     rec = {from, to, amount}
+//                 } else if (e.isV1050) {
+//                     let [from, to, amount] = e.asV1050
+//                     rec = {from, to, amount}
+//                 } else {
+//                     rec = e.asV9130
+//                 }
+//                 transfers.push({
+//                     id: item.event.id,
+//                     from: ss58.codec('kusama').encode(rec.from),
+//                     to: ss58.codec('kusama').encode(rec.to),
+//                     amount: rec.amount,
+//                     timestamp: BigInt(block.header.timestamp)
+//                 })
+//             }
+//         }
+//     }
+//     return transfers
+// }
+
+
+// function getAccount(m: Map<string, Account>, id: string): Account {
+//     let acc = m.get(id)
+//     if (acc == null) {
+//         acc = new Account()
+//         acc.id = id
+//         acc.balance = 0n
+//         m.set(id, acc)
+//     }
+//     return acc
+// }
